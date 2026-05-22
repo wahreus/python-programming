222ca4bfb0be1f4a064cf86435a7247eaf29d372
Johan Wahréus <johanwahreus1@gmail.com>
2026-05-22 16:16:08 +0200

    Modified heap_sort.py

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,13 +1,10 @@
 def heap_sort(numbers: list[int]) -> list[int]:
     heap_size = len(numbers)
-
     for i in range(heap_size // 2 - 1, -1, -1):
         heapify(numbers, heap_size, i)
-
     for i in range(heap_size - 1, 0, -1):
         numbers[0], numbers[i] = numbers[i], numbers[0]
         heapify(numbers, i, 0)
-
     return numbers
 
 
@@ -15,13 +12,10 @@ def heapify(numbers: list[int], heap_size: int, root_index: int) -> None:
     largest_index = root_index
     left_index = 2 * root_index + 1
     right_index = 2 * root_index + 2
-
     if left_index < heap_size and numbers[left_index] > numbers[largest_index]:
         largest_index = left_index
-
     if right_index < heap_size and numbers[right_index] > numbers[largest_index]:
         largest_index = right_index
-
     if largest_index != root_index:
         numbers[root_index], numbers[largest_index] = numbers[largest_index], numbers[root_index]
         heapify(numbers, heap_size, largest_index)
