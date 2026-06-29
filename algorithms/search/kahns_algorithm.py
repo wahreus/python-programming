eb3158556bcd155633e2c75b410245e38f777c66
Johan Wahréus <johanwahreus1@gmail.com>
2026-06-29 20:49:25 +0200

    Algorithm style edit

--- algorithms/search/kahns_algorithm.py
+++ algorithms/search/kahns_algorithm.py
@@ -4,39 +4,29 @@ from collections import deque
 def kahns_algorithm(graph: dict[str, list[str]]) -> list[str] | None:
     indegrees = _get_indegrees(graph)
     queue = deque()
-
     for node in graph:
         if indegrees[node] == 0:
             queue.append(node)
-
     order = []
-
     while queue:
         current = queue.popleft()
         order.append(current)
-
         for child in graph[current]:
             indegrees[child] -= 1
-
             if indegrees[child] == 0:
                 queue.append(child)
-
     if len(order) != len(graph):
         return None
-
     return order
 
 
 def _get_indegrees(graph: dict[str, list[str]]) -> dict[str, int]:
     indegrees = {}
-
     for node in graph:
         indegrees[node] = 0
-
     for node in graph:
         for child in graph[node]:
             indegrees[child] += 1
-
     return indegrees
 
 
